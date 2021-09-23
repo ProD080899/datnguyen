44e299133a0d6e0934a2cf29fc306d3446bd6635
ProD080899 <91022534+ProD080899@users.noreply.github.com>
2021-09-23 16:48:39 +0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include <Arduino.h>
 const int ledPin = 2;
+const int ledPin1 = 15;
+const int ledPin2 = 16;
 
 void setup() {
   // put your setup code here, to run once:
@@ -9,8 +11,12 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   digitalWrite(ledPin,0);
+    digitalWrite(ledPi1,1);
+    digitalWrite(ledPin2,0);
   delay(1500);
-   digitalWrite(ledPin,1);
+   digitalWrite(ledPin,1);  
+  digitalWrite(ledPin1,0);
+    digitalWrite(ledPin2,0);
   delay(1500);
 
 }
